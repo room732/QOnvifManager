af09537ea7581ffff9a02b1369c6ce7791a5f5f3
maxxant <maxxant@gmail.com>
2024-09-03 12:50:28 +0300

    main

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.hpp"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qWarning() << "start QOnvifManager";
     MainWindow w;
     w.show();
 
